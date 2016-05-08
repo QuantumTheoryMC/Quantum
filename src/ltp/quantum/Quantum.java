1204402e32656e628ac9047b8311c06c4a0f3933
LinkTheProgrammer <linktheprogrammer@gmail.com>
2016-05-08 10:41:56 -0400

    Rename package to ltp.quantum

---
+++ src/ltp/quantum/Quantum.java
@@ -0,0 +1,63 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 link.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * Created file on 3/21/16 at 1:29 PM.
+ *
+ * This file is part of Quantum API
+ */
+package ltp.quantum;
+
+/**
+ * @author link
+ */
+public final class Quantum {
+
+	static String API_VERSION, WRAPPER_VERSION, MC_VERSION, MC_DIRECTORY, MC_ASSETS;
+
+	private Quantum() {
+	}
+
+	public static void main(String... args) {
+		Bootstrap.run(args);
+	}
+
+	public static String getAPIVersion() {
+		return API_VERSION;
+	}
+
+	public static String getWrapperVersion() {
+		return WRAPPER_VERSION;
+	}
+
+	public static String getMinecraftVersion() {
+		return MC_VERSION;
+	}
+
+	public static String getMinecraftDir() {
+		return MC_DIRECTORY;
+	}
+
+	public static String getMinecraftAssetsDir() {
+		return MC_ASSETS;
+	}
+}
---
+++ src/ltp/quantum/util/RenderGroup.java
@@ -0,0 +1,49 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 link.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package ltp.quantum.util;
+
+import ltp.quantum.meta.WIP;
+import net.minecraft.util.EnumWorldBlockLayer;
+
+/**
+ * @author link
+ */
+@WIP(description = "For now, this is a placeholder. I will implement at the appropriate time.")
+public enum RenderGroup {
+
+	SOLID(EnumWorldBlockLayer.SOLID),
+	CUTOUT(EnumWorldBlockLayer.CUTOUT),
+	MIPPED_CUTOUT(EnumWorldBlockLayer.CUTOUT_MIPPED),
+	TRANSLUCENT(EnumWorldBlockLayer.TRANSLUCENT);
+
+	private final EnumWorldBlockLayer ewbl;
+
+	RenderGroup(EnumWorldBlockLayer ewbl) {
+		this.ewbl = ewbl;
+	}
+
+	EnumWorldBlockLayer getWrappedLayer() {
+		return ewbl;
+	}
+}
